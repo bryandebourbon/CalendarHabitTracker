a20adac3def1819ea2648551a5ae752204def28f
Bryan de Bourbon <bryandebourbon@gmail.com>
2023-11-27 22:02:02 -0500

    Initial Commit

---
+++ CalendarHabitTracker/CalendarHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarHabitTrackerApp.swift
+//  CalendarHabitTracker
+//
+//  Created by Bryan de Bourbon on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
